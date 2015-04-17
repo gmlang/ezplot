b00b8bd9ca6d1039982cbfe9c9e0119712f179c7
gmlang <rigor6vigor@gmail.com>
2015-04-16 23:53:22 -0400

    initial commit

---
+++ R/mk_barplot.R
@@ -0,0 +1,78 @@
+#' @title Create a function that draws ggplot2 barplots
+#' 
+#' @description
+#' \code{mk_barplot()} takes a data frame as input and returns a function that 
+#' can be used to make barplots on variables in the data frame.
+#' 
+#' @param df data frame containing variables to be visualized.
+#' @return
+#' \code{function(xvar, yvar, fillby, xlab="", ylab="", main="", ypct=F, ypct_jump=0.2, legend=T)}
+#' \itemize{
+#'      \item xvar     :  string, the x variable.
+#'      \item yvar     :  string, the y variable.
+#'      \item fillby   :  string, the variable used for coloring the bars.
+#'      \item xlab     :  string, the x-axis label.
+#'      \item ylab     :  string, the y-axis label.
+#'      \item main     :  string, the title of the plot. 
+#'      \item ypct     :  logical, indicating whether to use percent format on y-axis. Default is FALSE.
+#'      \item ypct_jump:  numeric, between 0 and 1. Default is 0.1
+#'      \item legend   :  logical, indicating whether to show the legend. Default is TRUE.
+#' }             
+#' @examples
+#' df = read.table(header=TRUE, text='
+#' student grade
+#' Joe 90
+#' Mary 75
+#' Alex 50')
+#' 
+#' barplt = mk_barplot(df)
+#' barplt("student", "grade", "student") 
+#' barplt("student", "grade", "student", legend=F) 
+#' 
+#' df2 = read.table(header=TRUE, text='
+#' group level val
+#' A      small 1.8
+#' A      medium 2.2
+#' A      large 1.5
+#' B      small 2.0
+#' B      medium 2.6
+#' B      large 1.0
+#' C      small 2.5
+#' C      medium 1.3
+#' C      large 2.9')
+#' 
+#' barplt = mk_barplot(df2)
+#' barplt("group", "val", "level") 
+#' 
+#' library(tidyr)
+#' library(dplyr)
+#' 
+#' # calculate the percentage of the levels within each group
+#' pct = df2 %>% spread(level, val)
+#' temp = pct[, 2:4]
+#' pct = cbind(group=pct[, 1], temp / apply(temp, 1, sum))
+#' pct = pct %>% gather(level, pct, -group)
+#' pct$level = factor(pct$level, levels=c("small", "medium", "large"))
+#' 
+#' # plot a stacked barplot to display the percentages
+#' barplt = mk_barplot(pct)
+#' barplt("group", "pct", "level", ypct=T)
+mk_barplot = function(df) {
+        function(xvar, yvar, fillby, xlab="", ylab="", main="", 
+                 ypct=F, ypct_jump=0.2, legend=T) {
+                p = ggplot2::ggplot(df, ggplot2::aes_string(x = xvar, y = yvar, fill = fillby)) + 
+                        ggplot2::geom_bar(stat = "identity") + 
+                        ggplot2::labs(x = xlab, y = ylab, title = main) +
+                        ggplot2::theme_bw()
+                                        
+                if (ypct)
+                        p = p + ggplot2::scale_y_continuous(labels = scales::percent, 
+                                                            limits = c(0,1),
+                                                            breaks = seq(0, 1, ypct_jump))
+                if (!legend)
+                        p = p + ggplot2::guides(fill = FALSE)
+                
+                p
+        }
+}
+
---
+++ R/palette.R
@@ -0,0 +1,19 @@
+#' The default colors in ggplot2 can be difficult to distinguish from one another 
+#' because they have equal luminance. They are also not friendly for colorblind 
+#' viewers. A good general-purpose solution is to just use the 
+#' colorblind-friendly palette below taken directly from 
+#' http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+#' 
+#' cbPalette is the palette with gray, and cbbPalette is with black.
+#' 
+common = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", 
+           "#CC79A7")
+cbPalette = c("#999999", common)
+cbbPalette = c("#000000", common)
+
+web_blue = "#569BBD"
+web_red = "#CC6666" 
+web_purple = "#9999CC"
+web_green = "#66CC99"
+
+        
