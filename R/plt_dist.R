b29edf2e9b2ece79f6e0c1c17728fed3828e562c
gmlang <rigor6vigor@gmail.com>
2015-04-17 21:52:48 -0400

    change element color in plt_dist()

--- R/plt_dist.R
+++ R/plt_dist.R
@@ -6,22 +6,19 @@
 #' 
 #' @param dat a data frame or a matrix with colnames.
 #' @param varname a string specifying the name of the variable of interest.
-#' 
 #' @return it renders 4 plots on one canvas using the base plot system.
-#' 
 #' @export
-#' 
 #' @examples
 #' plt_dist(cars, "speed")
 plt_dist = function(dat, varname) {
         par(mfrow = c(2,2))
         hist(dat[, varname], main="histogram", xlab=varname, probability=TRUE, 
-             col=hcl(h=195,l=65,c=100))
+             col="#569BBD")
         boxplot(dat[,varname], outchar=TRUE, main="boxplot", cex=0.7, 
-                xlab=varname, col=hcl(h=195,l=65,c=100))
+                xlab=varname, col="#569BBD")
         plot(density(dat[,varname]), type="l", main="Smoothed density", lwd=2,
-             xlab=varname, ylab="density estimate", col=hcl(h=195,l=65,c=100))
-        qqnorm(dat[,varname], col=hcl(h=195,l=65,c=100), cex=0.7)
+             xlab=varname, ylab="density estimate", col="#569BBD")
+        qqnorm(dat[,varname], col="#569BBD", cex=0.7, pch=19)
         qqline(dat[,varname])
         par(mfrow=c(1,1))
 }
