e12281584eb79555ab29fcacb8bbfe2a8763871b
gmlang <rigor6vigor@gmail.com>
2015-04-22 18:26:04 -0400

    add comma format to mk_barplot()

--- R/mk_barplot.R
+++ R/mk_barplot.R
@@ -7,7 +7,8 @@
 #' @param df data frame containing variables to be visualized.
 #' 
 #' @return
-#' \code{function(xvar, yvar, fillby, xlab="", ylab="", main="", ypct=F, ypct_jump=0.2, legend=T)}
+#' \code{function(xvar, yvar, fillby, xlab="", ylab="", main="", 
+#'                ylog=F, ylog10=F, ypct=F, ypct_jump=0.2, legend=T)}
 #' \itemize{
 #'      \item xvar     :  string, the x variable.
 #'      \item yvar     :  string, the y variable.
@@ -15,6 +16,8 @@
 #'      \item xlab     :  string, the x-axis label.
 #'      \item ylab     :  string, the y-axis label.
 #'      \item main     :  string, the title of the plot. 
+#'      \item ylog     :  logical, indicating whether to use log scale on y-axis. Default is FALSE.
+#'      \item ylog10   :  logical, indicating whether to use log10 scale on y-axis. Default is FALSE.
 #'      \item ypct     :  logical, indicating whether to use percent format on y-axis. Default is FALSE.
 #'      \item ypct_jump:  numeric, between 0 and 1. Default is 0.2
 #'      \item legend   :  logical, indicating whether to show the legend. Default is TRUE.
@@ -66,12 +69,21 @@
 #' barplt("group", "pct", "level", ypct=T)
 mk_barplot = function(df) {
         function(xvar, yvar, fillby, xlab="", ylab="", main="", 
-                 ypct=F, ypct_jump=0.2, legend=T) {
+                 ylog=F, ylog10=F, ypct=F, ypct_jump=0.2, legend=T) {
                 p = ggplot2::ggplot(df, ggplot2::aes_string(x = xvar, y = yvar, fill = fillby)) + 
                         ggplot2::geom_bar(stat = "identity") + 
                         ggplot2::labs(x = xlab, y = ylab, title = main) +
-                        ggplot2::theme_bw()
-                                        
+                        ggplot2::theme_bw() + ggplot2::scale_y_continuous(labels = scales::comma)
+                
+                if (ylog)
+                        p = p + ggplot2::scale_y_continuous(trans = scales::log_trans(),
+                                                            breaks = scales::trans_breaks("log", function(x) exp(x)),
+                                                            labels = scales::trans_format("log", scales::math_format('e'^.x)))
+                
+                if (ylog10)
+                        p = p + ggplot2::scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
+                                                       labels = scales::trans_format("log10", scales::math_format(10^.x)))
+                
                 if (ypct)
                         p = p + ggplot2::scale_y_continuous(labels = scales::percent, 
                                                             limits = c(0,1),
