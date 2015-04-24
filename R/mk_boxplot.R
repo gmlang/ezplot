23539d1af77c977d3c23d04a9965880fb154b626
gmlang <rigor6vigor@gmail.com>
2015-04-24 14:36:32 -0400

    update mk_boxplot()

--- R/mk_boxplot.R
+++ R/mk_boxplot.R
@@ -6,8 +6,7 @@
 #' 
 #' @param df data frame containing variables to be visualized.
 #' @return 
-#' \code{function(xvar, yvar, xlab="", ylab="", main="", vpos=0, 
-#'                ylog=F, ylog10=F, ypct=F, ypct_jump=0.2, legend=T)}
+#' \code{function(xvar, yvar, xlab="", ylab="", main="", vpos=0, legend=T)}
 #' \itemize{
 #'      \item xvar     :  string, the x variable.
 #'      \item yvar     :  string, the y variable.
@@ -15,10 +14,6 @@
 #'      \item ylab     :  string, the y-axis label.
 #'      \item main     :  string, the title of the plot. 
 #'      \item vpos     :  number, the additional height of the text labels beyond the max y-value of each group.
-#'      \item ylog     :  logical, indicating whether to use log scale on y-axis. Default is FALSE.
-#'      \item ylog10   :  logical, indicating whether to use log10 scale on y-axis. Default is FALSE.
-#'      \item ypct     :  logical, indicating whether to use percent format on y-axis. Default is FALSE.
-#'      \item ypct_jump:  number, between 0 and 1. Default is 0.2
 #'      \item legend   :  logical, indicating whether to show legend or not. Default is TRUE.
 #' }
 #' @export
@@ -28,31 +23,35 @@
 #' 
 #' # plot distributions of budget over the years
 #' title1 = "Annual Distribution of Budget from 1913 to 2014"
-#' p = plt("year", "budget", ylab="budget", main=title1, ylog10=T)
+#' p = plt("year", "budget", ylab="budget", main=title1)
+#' p = scale_axis(p, "y", use_comma=T)
 #' print(p)
+#' scale_axis(p, use_log10=T)
 #' 
 #' # plot distributions of boxoffice over the years
 #' title2 = "Annual Distribution of Boxoffice from 1913 to 2014"
-#' p = plt("year", "boxoffice", ylab="boxoffice", main=title2, ylog10=T)
+#' p = plt("year", "boxoffice", ylab="boxoffice", main=title2)
 #' start = min(films$year)
 #' end = max(films$year)
-#' p = p + ggplot2::scale_x_continuous(limits = c(start, end), breaks = seq(start, end, 10))
+#' p = scale_axis(p, "y", use_log10=T)
+#' p = p + ggplot2::scale_x_continuous(limits = c(start, end), 
+#'                                     breaks = seq(start, end, 10))
 #' print(p)
 #' 
 #' # plot distributions of boxoffice at each aggregated year level
-#' p = plt("year_cat", "boxoffice", ylab="boxoffice", ylog10=T)
+#' p = plt("year_cat", "boxoffice", ylab="boxoffice")
+#' p = scale_axis(p, use_log10=T)
 #' print(p)
 #' 
 #' # plot distributions of budget at each aggregated year level
-#' p = plt("year_cat", "budget", ylab="boxoffice", ylog10=T, legend=F)
-#' print(p)
+#' p = plt("year_cat", "budget", ylab="boxoffice", legend=F)
+#' scale_axis(p, use_log10=T)
 mk_boxplot = function(df) {
-        function(xvar, yvar, xlab="", ylab="", main="", vpos=0, 
-                 ylog=F, ylog10=F, ypct=F, ypct_jump=0.2, legend=T) {
-                
+        function(xvar, yvar, xlab="", ylab="", main="", vpos=0, legend=T) {
                 xvar_type = class(df[[xvar]])
                 if (xvar_type %in% c("character", "factor"))
-                        p = ggplot2::ggplot(df, ggplot2::aes_string(x=xvar, y=yvar, fill=xvar)) +
+                        p = ggplot2::ggplot(df, ggplot2::aes_string(x=xvar, y=yvar, 
+                                                                    fill=xvar)) +
                                 ggplot2::geom_boxplot() +
                                 ggplot2::stat_summary(fun.y=mean, geom="point", 
                                                       shape=5, size=2) +
@@ -60,25 +59,13 @@ mk_boxplot = function(df) {
                                         c(y = max(x) + vpos, label = length(x)), 
                                         geom = "text", size = 5) 
                 else
-                        p = ggplot2::ggplot(df, ggplot2::aes_string(x=xvar, y=yvar, group=xvar)) +
+                        p = ggplot2::ggplot(df, ggplot2::aes_string(x=xvar, y=yvar, 
+                                                                    group=xvar)) +
                                 ggplot2::geom_boxplot(color = palette("blue"))
                 
                 p = p + ggplot2::theme_bw() + 
                         ggplot2::labs(x = xlab, y = ylab, title = main)
-                
-                if (ylog)
-                        p = p + ggplot2::scale_y_continuous(trans = scales::log_trans(),
-                                                            breaks = scales::trans_breaks("log", function(x) exp(x)),
-                                                            labels = scales::trans_format("log", scales::math_format('e'^.x)))
-                else if (ylog10)
-                        p = p + ggplot2::scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
-                                                       labels = scales::trans_format("log10", scales::math_format(10^.x)))
-                else if (ypct)
-                        p = p + ggplot2::scale_y_continuous(labels = scales::percent, 
-                                                            limits = c(0, 1),
-                                                            breaks = seq(0, 1, ypct_jump))                
-                else p = p + ggplot2::scale_y_continuous(labels = scales::comma)
-                
+                                
                 if (!legend)
                         p = p + ggplot2::guides(fill = FALSE)
                 p
