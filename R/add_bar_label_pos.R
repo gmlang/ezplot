720436ce432bd9c2c95c4eb273e3bedd4576020d
gmlang <rigor6vigor@gmail.com>
2015-08-03 18:19:23 -0400

    clean up descriptions for add_bar_label_post()

--- R/add_bar_label_pos.R
+++ R/add_bar_label_pos.R
@@ -1,23 +1,23 @@
-#' @title Create a function that can be used to add two column of label 
-#' positions to a data frame.
+#' @title Append two columns of label positions to a data frame.
 #
 #' @description
-#' \code{add_bar_label_pos} can be used to prep data before making bar charts. 
-#' It takes a data frame as input and returns a function that can be used to 
-#' append columns to the same data frame. Specifically, the output function can 
-#' be used to add two extra columns to the original data frame, namely, pos_top 
-#' and pos_mid, where pos_top specify the positions when labeling the bars at 
-#' the top, and pos_mid specify the positions when labeling the bars at the
-#' middle.
+#' If you need to make a bar chart and label its bars, \code{add_bar_label_pos} 
+#' is your friend. It takes a data frame as input and returns a function that 
+#' can be used to append the positions of the labels to the same data frame. 
+#' Specifically, you can use the output function to add two extra columns,
+#' namely, pos_top and pos_mid, where pos_top specifies the positions of the 
+#' labels when placed on top of the bars, and pos_mid specifies the positions 
+#' of the labels when placed in the middle of the bars.
 #' 
-#' @param df a data frame containing variables to be visualized.
+#' @param df A data frame.
 #' 
 #' @return
 #' \code{function(xvar, yvar, vpos=0.2)}
 #' \itemize{
-#'      \item xvar     :  string, the x variable, often categorical.
-#'      \item yvar     :  string, the y variable, often Frequency/Count or Percent.
-#'      \item vpos     :  numeric, the extra vertical position beyond the top of each bar. Default is 0.2.
+#'      \item xvar     :  string, x variable, often categorical.
+#'      \item yvar     :  string, y variable, often Frequency/Count or Percent.
+#'      \item vpos     :  number, extra vertical distance beyond the topc of the 
+#'                        bars when placing above the bars. Default = 0.2.
 #' }
 #' 
 #' @seealso \code{\link{mk_barplot}}.
@@ -26,7 +26,8 @@
 #' # make some fake data
 #' year = c(rep(c("2006-07", "2007-08", "2008-09", "2009-10"), each = 4))
 #' cat  = c(rep(c("A", "B", "C", "D"), times = 4))
-#' freq = c(168, 259, 226, 340, 216, 431, 319, 368, 423, 645, 234, 685, 166, 467, 274, 251)
+#' freq = c(168, 259, 226, 340, 216, 431, 319, 368, 423, 645, 234, 685, 166, 
+#'          467, 274, 251)
 #' data = data.frame(year, cat, freq)
 #' data
 #' 
@@ -40,7 +41,6 @@ add_bar_label_pos = function(dat) {
         function(xvar, yvar, vpos=0.2) {
                 varname1 = paste(yvar, "pos_top", sep="_")
                 varname2 = paste(yvar, "pos_mid", sep="_")
-
                 lst = split(dat, dat[[xvar]])
                 lst_out = lapply(lst, function(elt) {
                         top_pos = cumsum(elt[[yvar]])
