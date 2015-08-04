df81455cf2f6cd34c1b192f1f80d62a5a0b5301f
gmlang <rigor6vigor@gmail.com>
2015-08-03 20:32:01 -0400

    clean up description for mk_scatterplot()

--- R/mk_scatterplot.R
+++ R/mk_scatterplot.R
@@ -1,24 +1,24 @@
 #' @title Create a function that draws ggplot2 scatterplots.
 #' 
 #' @description
-#' \code{mk_scatterplot()} takes a data frame as input and returns a function that 
-#' can be used to make scatterplots on variables in the data frame.
+#' \code{mk_scatterplot} takes a data frame as input and returns a function that 
+#' can be used to make scatterplots using variables in the data frame.
 #' 
-#' @param df data frame containing variables to be visualized.
+#' @param df A data frame.
 #' @return 
-#' \code{function(xvar, yvar, fillby="", xlab="", ylab="", main="", add_line=F,
+#' \code{function(xvar, yvar, fillby="", xlab="", ylab="", main="", add_line=FALSE,
 #'                linew=1, pt_alpha=0.5, pt_size=1)}
 #' \itemize{
-#'      \item xvar     :  string, the x variable.
-#'      \item yvar     :  string, the y variable.
-#'      \item fillby   :  string, the variable used for coloring the points
-#'      \item xlab     :  string, the x-axis label.
-#'      \item ylab     :  string, the y-axis label.
-#'      \item main     :  string, the title of the plot. 
-#'      \item add_line :  logical, indicating whether to add a lm line. Default is F.
-#'      \item linew    :  numeric, default is 1.
-#'      \item pt_alpha :  numeric, specifying the transparency of the points. Default is 0.5.
-#'      \item pt_size  :  numeric, specifying the size of the points. Default is 1.
+#'      \item xvar     :  string, x variable.
+#'      \item yvar     :  string, y variable.
+#'      \item fillby   :  string, variable used for coloring the points
+#'      \item xlab     :  string, x-axis label.
+#'      \item ylab     :  string, y-axis label.
+#'      \item main     :  string, title of the plot. 
+#'      \item add_line :  logical, whether to add a lm line. Default = FALSE.
+#'      \item linew    :  number, default = 1.
+#'      \item pt_alpha :  number, transparency of the points. Default = 0.5.
+#'      \item pt_size  :  number, size of the points. Default = 1.
 #' }
 #' 
 #' @export
@@ -31,23 +31,23 @@
 #' 
 #' p = plt("budget", "boxoffice", xlab="budget", ylab="boxoffice", 
 #'          pt_alpha=0.2, pt_size=1.5, add_line=T, linew=0.8)
-#' scale_axis(p, "y", use_log=T)
-#' scale_axis(p, "x", use_log=T)
+#' scale_axis(p, "y", scale = "log")
+#' scale_axis(p, "x", scale = "log")
 #' 
-#' p = scale_axis(p, "x", use_log10=T)
-#' p = scale_axis(p, "y", use_log10=T)
+#' p = scale_axis(p, "x", scale = "log10")
+#' p = scale_axis(p, "y", scale = "log10")
 #' print(p)
 #' 
 #' p = plt("budget", "boxoffice", fillby="year_cat", xlab="budget", 
 #'          ylab="boxoffice", add_line=T)
-#' p = scale_axis(p, "x", use_log10=T)
-#' p = scale_axis(p, "y", use_log10=T)
+#' p = scale_axis(p, "x", scale = "log10")
+#' p = scale_axis(p, "y", scale = "log10")
 #' print(p)
 #' 
 #' p = plt("budget", "boxoffice", fillby="made_money", xlab="budget", 
 #'          ylab="boxoffice", add_line=T)
-#' p = scale_axis(p, "x", use_log10=T)
-#' p = scale_axis(p, "y", use_log10=T)
+#' p = scale_axis(p, "x", scale = "log2")
+#' p = scale_axis(p, "y", scale = "log2")
 #' print(p)
 mk_scatterplot = function(df) {
         function(xvar, yvar, fillby="", xlab="", ylab="", main="",
