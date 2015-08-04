53239ef14cb05a38643862ed750bc36f01a82de8
gmlang <rigor6vigor@gmail.com>
2015-08-03 20:16:41 -0400

    clean up description for mk_lineplot()

--- R/mk_lineplot.R
+++ R/mk_lineplot.R
@@ -1,23 +1,23 @@
-#' @title Create a function that draws ggplot2 lineplots connectiong points
+#' @title Create a function that draws ggplot2 line plots.
 #' 
 #' @description
-#' \code{mk_lineplot()} takes a data frame as input and returns a function that 
-#' can be used to make lineplots or multi-lineplots on variables in the data frame.
-#' The variable on the x-axis often measures time, and can be categorical.
+#' \code{mk_lineplot} takes a data frame as input and returns a function that 
+#' can be used to make line plots using variables in the data frame.
+#' The x variable often measures time, and it can be categorical.
 #' 
-#' @param df data frame containing variables to be visualized.
+#' @param df A data frame.
 #' @return 
 #' \code{function(xvar, yvar, fillby="", xlab="", ylab="", main="", linew=0.7, 
 #'                pt_size=2)}
 #' \itemize{
-#'      \item xvar     :  string, the x variable.
-#'      \item yvar     :  string, the y variable.
-#'      \item fillby   :  string, the variable used for coloring the lines and points.
-#'      \item xlab     :  string, the x-axis label.
-#'      \item ylab     :  string, the y-axis label.
-#'      \item main     :  string, the title of the plot. 
-#'      \item linew    :  numeric, the width of the lines.
-#'      \item pt_size  :  numeric, the size of the points.
+#'      \item xvar     :  string, x variable.
+#'      \item yvar     :  string, y variable.
+#'      \item fillby   :  string, variable used for coloring the lines and points.
+#'      \item xlab     :  string, x-axis label.
+#'      \item ylab     :  string, y-axis label.
+#'      \item main     :  string, title of the plot. 
+#'      \item linew    :  numeric, width of the lines.
+#'      \item pt_size  :  numeric, size of the points.
 #' }   
 #' 
 #' @export
@@ -28,10 +28,10 @@
 #' plt = mk_lineplot(bo_bt_ratio_by_year)
 #' title = "Boxoffice/Budget Ratio from 1913 to 2014"
 #' p = plt("year", "bo_bt_ratio", ylab="boxoffice/budget ratio", main=title)
-#' scale_axis(p, use_log10=T)
+#' scale_axis(p, scale="log10")
 #' 
 #' p = plt("year", "bo_bt_ratio", ylab="boxoffice/budget ratio", linew=1, pt_size=2)
-#' scale_axis(p, use_log10=T)
+#' scale_axis(p, scale="log10")
 #' 
 #' # plot total budget and boxoffice over the years
 #' plt = mk_lineplot(btbo_by_year)
