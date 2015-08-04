562b959541b2f3dfa75912b7dcf34bbbc0f8470c
gmlang <rigor6vigor@gmail.com>
2015-08-03 20:43:38 -0400

    clean up description of rotate_axis_text()

--- R/rotate_axis_text.R
+++ R/rotate_axis_text.R
@@ -1,10 +1,17 @@
 #' @title Rotate axis tick text
 #' 
-#' @param p ggplot2 object.
-#' @param axis_text_angle_x the angle of the tick text on x-axis. Default=45.
-#' @param axis_text_angle_y the angle of the tick text on y-axis. Default=0.     
-#' @return a ggplot2 object.
+#' @param p A ggplot2 object.
+#' @param text_angle_x Angle of tick text on x-axis. Default=45.
+#' @param text_angle_y Angle of tick text on y-axis. Default=0. 
+#' @param hjust_x Vertical distance adjustment of the tick text to x-axis. Default=NULL.
+#' @param hjust_y Horizontal distance adjustment of the tick text to y-axis. Default=NULL.
+#' @param vjust_x Horizontal distance adjustment of the tick text to x-axis. Default=NULL.
+#' @param vjust_y Vertical distance adjustment of the tick text to y-axis. Default=NULL.
+#'     
+#' @return A ggplot2 object.
+#' 
 #' @export
+#' 
 #' @examples
 #' # make some data
 #' df = read.table(header=TRUE, text='
@@ -21,7 +28,7 @@
 #' # rotate the x-axis tick text 
 #' rotate_axis_text(p)
 #' rotate_axis_text(p, 90)
-#' rotate_axis_text(p, 90, 30)
+#' rotate_axis_text(p, 90, 30, vjust_x=-2)
 rotate_axis_text = function(p, text_angle_x=45, text_angle_y=0,
                             hjust_x=NULL, hjust_y=NULL, 
                             vjust_x=NULL, vjust_y=NULL) {
