8c4308df8f43d59ff379f6eebbad8858fc7a881b
gmlang <rigor6vigor@gmail.com>
2015-08-03 20:45:48 -0400

    clean up description for format_as_pct()

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,16 @@
 #' @title Format numbers as percent.
 #
 #' @description
-#' \code{format_as_pct()} takes a numeric argument \code{x} and returns a 
-#' character vector containing the original numeric vector in percent format.
+#' \code{format_as_pct} takes a numeric argument and returns a 
+#' character vector of the original numeric vector in percent format.
 #' 
-#' @param x a numeric vector.
-#' @param digits integer indicating the number of decimal places to be used.
-#' @return a character vector that's just the original vector in percent format.
+#' @param x A numeric vector.
+#' @param digits An integer indicating the number of decimal places to be used.
+#' 
+#' @return A character vector that's just x in percent format.
+#' 
 #' @export
+#' 
 #' @examples
 #' a = c(0.84984554, 0.18, 0.9273, 0.128294)
 #' format_as_pct(a)
