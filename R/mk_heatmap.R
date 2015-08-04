fae7fa6262e0a7d37ebe2a644949b624651f642f
gmlang <rigor6vigor@gmail.com>
2015-08-03 20:13:15 -0400

    clean up mk_heatmap()

--- R/mk_heatmap.R
+++ R/mk_heatmap.R
@@ -1,30 +1,28 @@
-#' @title Create a function that draws ggplot2 heatmaps
+#' @title Create a function that draws ggplot2 heat maps.
 #
 #' @description
-#' \code{mk_heatmap()} takes a data frame as input and returns a function that 
-#' can be used to make heatmaps on variables in the data frame.
+#' \code{mk_heatmap} takes a data frame as input and returns a function that 
+#' can be used to make heat maps using variables in the data frame.
 #' 
-#' @param df data frame containing variables to be visualized.
+#' @param df A data frame.
 #' 
 #' @return
 #' \code{function(xvar, yvar, fillby, xlab="", ylab="", main="", base_size = 12, 
-#'                use_theme_gray=T, legend=T)}
+#'                use_theme_gray=TRUE, legend=TRUE)}
 #' \itemize{
-#'      \item xvar     :  string, the x variable.
-#'      \item yvar     :  string, the y variable.
-#'      \item fillby   :  string, the variable used for coloring the bars.
-#'      \item xlab     :  string, the x-axis label.
-#'      \item ylab     :  string, the y-axis label.
-#'      \item main     :  string, the title of the plot. 
-#'      \item base_size:  numeric, the size of both axis tick text, default = 12.
-#'      \item use_theme_gray:  logical, indicating whether to use theme_gray() or theme_minimal(). Default is TRUE.
-#'      \item legend   :  logical, indicating whether to show the legend. Default is TRUE.
+#'      \item xvar     :  string, x variable.
+#'      \item yvar     :  string, y variable.
+#'      \item fillby   :  string, variable used for coloring the bars.
+#'      \item xlab     :  string, x-axis label.
+#'      \item ylab     :  string, y-axis label.
+#'      \item main     :  string, title of the plot. 
+#'      \item base_size:  numeric, size of tick text for both axes. Default = 12.
+#'      \item use_theme_gray:  logical, whether to use theme_gray or theme_minimal. Default = TRUE.
+#'      \item legend   :  logical, whether to show the legend. Default = TRUE.
 #' }    
 #'      
 #' @export
 #'     
-#' @examples
-#' # make some fake data
 mk_heatmap = function(df) {
         function(xvar, yvar, fillby, xlab="", ylab="", main="", base_size = 12, 
                  use_theme_gray=T, legend=T) {
@@ -35,8 +33,10 @@ mk_heatmap = function(df) {
                 else 
                         p = p + ggplot2::theme_minimal(base_size=base_size)
                 
-                p = p + ggplot2::geom_tile(ggplot2::aes_string(fill=fillby), color="white") + 
-                        ggplot2::scale_fill_gradient(low="white", high="steelblue") +
+                p = p + ggplot2::geom_tile(ggplot2::aes_string(fill=fillby), 
+                                           color="white") + 
+                        ggplot2::scale_fill_gradient(low="white", 
+                                                     high="steelblue") +
                         ggplot2::labs(x=xlab, y=ylab, title=main) +
                         ggplot2::scale_x_discrete(expand = c(0, 0)) +
                         ggplot2::scale_y_discrete(expand = c(0, 0)) +
