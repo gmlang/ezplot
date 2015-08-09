f41a508ce90f723dc4d2fca33b7e319afe33bd2a
gmlang <rigor6vigor@gmail.com>
2015-08-09 12:40:56 -0400

    add mk_areaplot()

--- R/mk_areaplot.R
+++ R/mk_areaplot.R
@@ -1,37 +1,46 @@
-    
-
+#' @title Create a function that draws ggplot2 stacked area chart.
+#
+#' @description
+#' \code{mk_areaplot} takes a data frame as input and returns a function that 
+#' can be used to make stacked area plot using the variables in the data frame.
+#' 
+#' @param df A data frame.
+#' 
+#' @return
+#' \code{function(xvar, yvar, fillby, xlab="", ylab="", main="", legend=T)}
+#' \itemize{
+#'      \item xvar     :  string, the x variable.
+#'      \item yvar     :  string, the y variable.
+#'      \item fillby   :  string, the variable used for coloring the bars.
+#'      \item xlab     :  string, the x-axis label.
+#'      \item ylab     :  string, the y-axis label.
+#'      \item main     :  string, the title of the plot. 
+#'      \item legend   :  logical, whether to show the legend. Default is TRUE.
+#' }
+#' 
+#' @seealso \code{\link{scale_axis}} for adding different scales to the axes. 
+#' 
+#' @export
+#' 
+#' @examples    
+#' library(ezplot)
+#' library(tidyr)
+#' library(dplyr)
+#' 
+#' dat = ads %>% gather(cat, rev, -year)
+#' plt = mk_areaplot(dat)
+#' plt("year", "rev", fillby = "cat")
 mk_areaplot = function(df) {
-        function(xvar, yvar, fillby, 
-                 xlab="", ylab="", main="", legend=T, 
-                 arealab=NULL, arealab_use_pct=F, decimals=2,
-                 arealab_at_top=F, arealab_size=3) {
-                
+        function(xvar, yvar, fillby, xlab="", ylab="", main="", legend=T) {
                 p = ggplot2::ggplot(df, ggplot2::aes_string(x = xvar, y = yvar, 
                                                             fill = fillby,
                                                             order = fillby)) + 
                         ggplot2::geom_area(position = "stack") + 
                         ggplot2::labs(x = xlab, y = ylab, title = main) +
                         ggplot2::theme_bw() +
-                        ggplot2::guides(fill = ggplot2::guide_legend(reverse=TRUE))
+                        ggplot2::guides(fill=ggplot2::guide_legend(reverse=TRUE))
                 
                 if (!legend) p = p + ggplot2::guides(fill = FALSE)
-                
-                if (!is.null(arealab)) {  
-                        if (arealab_use_pct) 
-                                df$label = format_as_pct(df[[arealab]], 
-                                                             digits=decimals+2)
-                        else df$label = df[[arealab]]
-                        
-                        if (arealab_at_top)
-                                arealab_pos = paste(yvar, "pos_top", sep="_")
-                        else arealab_pos = paste(yvar, "pos_mid", sep="_")
-                        
-                        p = p + ggplot2::geom_text(data=df, 
-                                                   ggplot2::aes_string(
-                                                           label = "label",
-                                                           y = arealab_pos), 
-                                                   size = arealab_size)
-                }
                 p
         }
 }
