58c4b44c323ed98a7529769baa2269ff986c03a1
gmlang <rigor6vigor@gmail.com>
2015-09-05 17:34:45 -0400

    add missing_fixer()

--- R/utils.R
+++ R/utils.R
@@ -15,3 +15,36 @@
 #' a = c(0.84984554, 0.18, 0.9273, 0.128294)
 #' format_as_pct(a)
 format_as_pct = function(x, digits=4) paste0(round(x, digits) * 100, "%")
+
+
+#' @title Change missing value code to NA.
+#
+#' @description
+#' \code{missing_fixer} takes a number or a string that represents missing value
+#' and returns a function that can be used to replace the number or string 
+#' occurred in a vector with NA.
+#' 
+#' @param na_value A number or a string.
+#' 
+#' @return 
+#' \code{function(x)}
+#' \itemize{
+#'      \item x     :  a numeric or character vector.
+#' }
+#' 
+#' @export
+#' 
+#' @examples
+#' fix_missing_99 = missing_fixer(-99)
+#' fix_missing_999 = missing_fixer(-999)
+#' fix_missing_char = missing_fixer("missing")
+#' 
+#' fix_missing_99(c(-99, -999))
+#' fix_missing_999(c(-99, -999))
+#' fix_missing_char(c("missing", "male", "female"))
+missing_fixer = function(na_value) {
+        function(x) {
+                x[x == na_value] = NA
+                x
+        }
+}
