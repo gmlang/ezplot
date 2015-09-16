907e6e12991e16d16140da4f22db5551f8ec7ecb
gmlang <rigor6vigor@gmail.com>
2015-09-16 09:46:49 -0400

    update boxplot

--- R/mk_boxplot.R
+++ R/mk_boxplot.R
@@ -6,16 +6,18 @@
 #' 
 #' @param df A data frame.
 #' @return 
-#' \code{function(xvar, yvar, xlab="", ylab="", main="", vpos=0, legend=TRUE)}
+#' \code{function(xvar, yvar, xlab="", ylab="", main="", legend=TRUE, 
+#'                add_label=TRUE, lab_at_top=TRUE, vpos=0)}
 #' \itemize{
 #'      \item xvar     :  string, x variable.
 #'      \item yvar     :  string, y variable.
 #'      \item xlab     :  string, x-axis label.
 #'      \item ylab     :  string, y-axis label.
-#'      \item lab_at_top: logical, if T, label the boxplots at the top. Default is TRUE.
 #'      \item main     :  string, title of the plot. 
-#'      \item vpos     :  number, additional height of the text labels beyond the max y-value of each group.
 #'      \item legend   :  logical, show legend or not. Default = TRUE.
+#'      \item add_label:  logical, whether or not to put number of observations as labels. Default is TRUE.
+#'      \item lab_at_top: logical, whether to put the labels at the top or bottom. Default is TRUE.
+#'      \item vpos     :  number, additional height of the text labels beyond the max y-value of each group.
 #' }
 #' @export
 #' @examples 
@@ -48,9 +50,10 @@
 #' p = plt("year_cat", "budget", ylab="boxoffice", legend=F)
 #' scale_axis(p, scale="log10")
 mk_boxplot = function(df) {
-        function(xvar, yvar, xlab="", ylab="", main="", lab_at_top=T,
-                 vpos=0, legend=T) {
+        function(xvar, yvar, xlab="", ylab="", main="", legend=T, 
+                 add_label=T, lab_at_top=T, vpos=0) {
                 xvar_type = class(df[[xvar]])
+                # make plot
                 if (xvar_type %in% c("character", "factor"))
                         p = ggplot2::ggplot(df, ggplot2::aes_string(x=xvar, y=yvar, fill=xvar)) +
                                 ggplot2::geom_boxplot() +
@@ -60,20 +63,23 @@ mk_boxplot = function(df) {
                                 ggplot2::geom_boxplot(color = cb_color("blue"))
                 
                 # label with count of observations
-                if (lab_at_top)
-                        p = p + ggplot2::stat_summary(fun.data = function(x) 
-                                c(y = max(x) + vpos, label = length(x)), 
-                                geom = "text", size = 5)
-                else 
-                        p = p + ggplot2::stat_summary(fun.data = function(x) 
-                                c(y = min(x) + vpos, label = length(x)), 
-                                geom = "text", size = 5)
+                if (add_label) {
+                        if (lab_at_top)
+                                p = p + ggplot2::stat_summary(fun.data = function(x) 
+                                        c(y = max(x) + vpos, label = length(x)), 
+                                        geom = "text", size = 5)
+                        else 
+                                p = p + ggplot2::stat_summary(fun.data = function(x) 
+                                        c(y = min(x) + vpos, label = length(x)), 
+                                        geom = "text", size = 5)
+                }
                 
+                # annotate plot
                 p = p + ggplot2::theme_bw() + 
                         ggplot2::labs(x = xlab, y = ylab, title = main)
-                                
                 if (!legend)
                         p = p + ggplot2::guides(fill = FALSE)
+                
                 p
         }
 }
