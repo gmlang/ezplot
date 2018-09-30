2ce94215139a774ac9d26e64a351520dffdb6c8c
Guangming Lang <rigor6vigor@gmail.com>
2018-09-30 17:49:40 +0700

    remove all files on master branch

--- R/ezplot-package.R
+++
@@ -1,9 +0,0 @@
-#' ezplot
-#'
-#' This package contains easy-to-use functions for creating the most used
-#' ggplot2 charts with publishable quality.
-#'
-#' @name ezplot
-#' @docType package
-#' @import ggplot2
-NULL
