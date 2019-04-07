639c02e121a29f85f24b2583b110e61c633f5157
Guangming Lang <rigor6vigor@gmail.com>
2019-04-07 21:45:51 +0900

    add an extra parameter "show" to add_lm_line() to allow display of table of detailed quantatities of the best fit line

--- R/add_lm_line.R
+++ R/add_lm_line.R
@@ -1,62 +1,101 @@
-#' @title Add the best line with equation, R-squared and p-value to scatterplot.
+#' @title Add the best line with (equation, R-squared, p-value) or
+#' (fit table, R-squared) to scatterplot.
 #'
 #' @description
 #' \code{add_lm_line} takes a ggplot object (scatterplot) as input and adds
-#' the best line to the plot with equation, R-squared and p-value of the x term
-#' shown as text labels. (Note: because it's simple linear regression, the
-#' p-value is the same as the p-value obtained when running correlation test,
-#' and the R-squared value is just the correlation squared.)
+#' the best line to the plot with its equation, the R-squared value, and the
+#' p-value of the x term coef. estimate. (Note: because it's simple linear
+#' regression, the p-value is the same as the p-value obtained when running
+#' correlation test, and the R-squared value is just the correlation squared.)
+#' Alternatively, by settting \code{show = "tb"}, user can add to the plot the
+#' entire fit table (term, coef. estimate, standard error, tstat and pval) and
+#' the R-squared value.
 #' If the input scatterplot object has 1+ colored points, it will add 1+ lines
-#' with each line corresponding to a color.
+#' with each line corresponding to a color. When this happens (when the
+#' scatterplot is drawn with a fillby variable), use the default
+#' \code{show = "eq"} since printing 1+ fit tables doesn't work.
 #'
 #' @param p : a ggplot object, must be a scatter plot.
 #' @param alpha : a number between 0 and 1, transparency level of the best line.
 #'                Smaller value means more transparent. Default = 0.8.
 #' @param linew : number, width of the line. Default = 1.
-#' @param eq_xpos: numeric between 0 and 1 or character. x-position of the
-#'                 equation label. Default = "left".
-#' @param eq_ypos: numeric between 0 and 1 or character. y-position of the
-#'                 equation label. Default = "top".
-#' @param pval_xpos: numeric between 0 and 1 or character. x-position of the
-#'                   p-value label. Default = "right".
-#' @param pval_ypos: numeric between 0 and 1 or character. y-position of the
-#'                   p-value label. Default = "bottom".
-#' @param ...: other arguments for ggpmisc::stat_poly_eq(). For example,
-#'             coef.digits sets the number of param coef digits,
-#'             rr.digits sets the number of R2 digits,
-#'             label.x.npc and label.y.npc set position of the line equation.
+#' @param show  : string, "eq" or "tb". If "eq" (default), add the equation of
+#' the best fit line, the R-squared value, and the p-value of the x term coef.
+#' estimate to the plot. If "tb", add the fit table and the R-squared value to
+#' the plot.
+#' @param eq_tb_xpos: numeric between 0 and 1 or character. x-position of the
+#' equation or fit table label. Default = "left".
+#' @param eq_tb_ypos: numeric between 0 and 1 or character. y-position of the
+#' equation or fit table label. Default = "top".
+#' @param pv_r2_xpos: numeric between 0 and 1 or character. x-position of the
+#' p-value or R-squared value label. Default = "right".
+#' @param pv_r2_ypos: numeric between 0 and 1 or character. y-position of the
+#' p-value or R-squared value label. Default = "bottom".
+#' @param ...: other arguments for ggpmisc::stat_poly_eq() or
+#' ggpmisc::stat_fit_tb(). For example, for ggpmisc::stat_poly_eq(),
+#' coef.digits sets the number of digits for coef. estimates,
+#' rr.digits sets the number of digits for R-squared value.
+#' For ggpmisc::stat_fit_tb(), digits sets the digits for all numbers in the fit
+#' table.
 #'
-#' @return a ggoplot object with the best line with equation, R-squared and
-#'         p-value added.
+#' @return a ggplot scatterplot object with the best line added with detailed
+#' labels.
 #'
 #' @export
 #' @examples inst/examples/ex-add_lm_line.R
-add_lm_line = function(p, alpha = 0.8, linew = 1,
-                       eq_xpos = "left", eq_ypos = "top",
-                       pval_xpos = "right", pval_ypos = "bottom", ...)
-        p + ggplot2::geom_line(stat = "smooth", method = "lm",
-                               alpha = alpha, size = linew) +
-                ggpmisc::stat_poly_eq(
-                        ggplot2::aes(label = paste(..eq.label.., ..rr.label..,
-                                                   sep = "*\",\"~~")),
-                        formula = y ~ x,
-                        parse = TRUE, # allows italic to work
-                        label.x.npc = eq_xpos, label.y.npc = eq_ypos,
-                        ...) +
-                ggpmisc::stat_fit_glance(
-                        ggplot2::aes(
-                                label = paste('~italic(p)~-value',
-                                              ifelse(..p.value.. < 0.001,
-                                                     "< 0.001",
-                                                     paste('~"="~',
-                                                           round(..p.value.., 3)
-                                                           )
-                                                     )
-                                              )
+add_lm_line = function(p, alpha = 0.8, linew = 1, show = "eq",
+                       eq_tb_xpos = "left", eq_tb_ypos = "top",
+                       pv_r2_xpos = "right", pv_r2_ypos = "bottom", ...) {
 
-                                ),
-                        parse = T, # allows italic to work
-                        geom = "text",
-                        method = "lm",
-                        label.x.npc = pval_xpos, label.y.npc = pval_ypos
-                        )
+        p = p + ggplot2::geom_line(stat = "smooth", method = "lm",
+                                   alpha = alpha, size = linew)
+
+        if (show == "eq")
+                p = p + ggpmisc::stat_poly_eq(
+                                ggplot2::aes(
+                                        label = paste(..eq.label..,
+                                                      ..rr.label..,
+                                                      sep = "*\",\"~~")
+                                        ),
+                                formula = y ~ x,
+                                parse = TRUE, # allows italic to work
+                                label.x = eq_tb_xpos,
+                                label.y = eq_tb_ypos, ...) +
+                        ggpmisc::stat_fit_glance(
+                                ggplot2::aes(
+                                        label = paste('~italic(p)~-value',
+                                                      ifelse(..p.value.. < 0.001,
+                                                             "< 0.001",
+                                                             paste('~"="~', round(..p.value.., 3))
+                                                             ))
+                                        ),
+                                parse = TRUE, # allows italic to work
+                                label.x = pv_r2_xpos,
+                                label.y = pv_r2_ypos)
+
+        if (show == "tb")
+                p = p + ggpmisc::stat_fit_tb(
+                                tb.type = "fit.summary",
+                                tb.vars = c("term",
+                                            "est" = "estimate",
+                                            "SE" = "std.error",
+                                            "~italic(t)~stat" = "statistic",
+                                            "~italic(p)~val" = "p.value"),
+                                parse = TRUE, # allows italic to work
+                                label.x = eq_tb_xpos,
+                                label.y = eq_tb_ypos, ...) +
+                        ggpmisc::stat_fit_glance(
+                                ggplot2::aes(
+                                        label = sprintf('~italic(R^2)~"="~%.3f',
+                                                        ..r.squared..)),
+                                parse = TRUE, # allows italic to work
+                                label.x = pv_r2_xpos,
+                                label.y = pv_r2_ypos)
+
+        p
+
+}
+
+
+
+
--- inst/examples/ex-add_lm_line.R
+++ inst/examples/ex-add_lm_line.R
@@ -1,28 +1,31 @@
+library(dplyr)
 library(ezplot)
 
 plt = mk_scatterplot(films)
 
-p = plt("budget", "boxoffice") %>%
-        add_labs(xlab = "Budget", ylab = "Boxoffice")
+p = plt("budget", "boxoffice") %>% add_labs(xlab = "Budget", ylab = "Boxoffice")
 add_lm_line(p)
-add_lm_line(p, coef.digits = 8, rr.digits = 3, eq_xpos = "right")
-add_lm_line(p, eq_xpos = "right", pval_ypos = 0.8)
+add_lm_line(p, eq_tb_xpos = "right", pv_r2_ypos = 0.6)
+add_lm_line(p, show = "tb", eq_tb_xpos = "right", pv_r2_ypos = 0.6, digits = 2)
 
 p = scale_axis(p, scale = "log10")
 p = scale_axis(p, "x", scale = "log10")
-ggplot2::ggsave("tmp.pdf", add_lm_line(p, coef.digits = 3, rr.digits = 3))
-add_lm_line(p, coef.digits = 3, rr.digits = 3,
-            pval_xpos = "left", pval_ypos = "top", vjust = 0)
+add_lm_line(p, coef.digits = 3, rr.digits = 3)
+add_lm_line(p, show = "tb", pv_r2_xpos = "left", pv_r2_ypos = "top",
+            vjust = 1.5, digits = 4)
 
 p = plt("budget", "boxoffice", fillby = "made_money")
-add_lm_line(p, coef.digits = 8, rr.digits = 3, eq_xpos = "right",
-            pval_ypos = 0.8)
+add_lm_line(p, coef.digits = 8, rr.digits = 3, eq_tb_xpos = "right")
+
+# Don't show fit table when having a fillby because the fit table is the
+# overall one (i.e., the fillby variable doesn't affect the fit table).
+add_lm_line(p, show = "tb", digits = 4) # avoid doing this!
+
 p = scale_axis(p, scale = "log")
 p = scale_axis(p, "x", scale = "log")
-add_lm_line(p, coef.digits = 3, rr.digits = 3)
+add_lm_line(p)
 
 p = plt("rating", "bo_bt_ratio", fillby = "made_money") %>%
         add_labs(ylab = "Boxoffice / Budget Ratio")
 add_lm_line(p, coef.digits = 3, rr.digits = 3)
-p %>% scale_axis(scale = "log10") %>%
-        add_lm_line(coef.digits = 3, rr.digits = 3)
+p %>% scale_axis(scale = "log10") %>% add_lm_line(coef.digits = 3, rr.digits = 3)
