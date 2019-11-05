89760f8488003a3e8ca1d7fcc06f551dd27c8f43
Guangming Lang <rigor6vigor@gmail.com>
2019-11-05 18:01:02 +0900

    change `fillby` to 'colorby' since the latter naming makes more sense for line plots.

--- R/mk_lineplot.R
+++ R/mk_lineplot.R
@@ -5,29 +5,29 @@
 #' making lineplots with any continuous variables from the data frame on the y
 #' axis, and any continuous or categorical variables from the data frame on the
 #' x axis. The x variable often measures time, for example, years. When supplied
-#' a categorical fillby variable, the output function will produce lineplots
-#' where lines are colored differently according to the levels of the fillby
+#' a categorical colorby variable, the output function will produce lineplots
+#' where lines are colored differently according to the levels of the colorby
 #' variable.
 #'
 #' @param df A data frame.
 #' @return
-#' \code{function(xvar, yvar, fillby = "1", pt_size = 1, linew = 0.7,
-#'                add_cnt_to_legend = TRUE, legend_title = fillby,
+#' \code{function(xvar, yvar, colorby = "1", pt_size = 1, linew = 0.7,
+#'                add_cnt_to_legend = TRUE, legend_title = colorby,
 #'                legend_pos = "right", font_size = 14)}
 #' \itemize{
 #'      \item xvar   : string, name of a continuous or categorical variable for
 #'                     x-axis.
 #'      \item yvar   : string, name of a continuous variable for y-axis.
-#'      \item fillby : string, name of a categorical variable for
+#'      \item colorby : string, name of a categorical variable for
 #'                     grouping and coloring the lines. Default = "1", meaning
 #'                     no such variable is supplied.
 #'      \item pt_size: number, size of the points. Default = 1.
 #'      \item linew  : number, width of the line. Default = 0.7.
 #'      \item add_cnt_to_legend: logical, when TRUE (default), it will show
 #'                    the number of non-missing records for each level in the
-#'                    fillby var.
+#'                    colorby var.
 #'      \item legend_title: string, legend title. Default uses the name of the
-#'               fillby variable.
+#'               colorby variable.
 #'      \item legend_pos:   string, legend position. Default = "right".
 #'      \item font_size: overall font size. Default = 14. The font size of the
 #'                       axes and legend text is a fraction of this value.
@@ -36,8 +36,8 @@
 #' @export
 #' @examples inst/examples/ex-mk_lineplot.R
 mk_lineplot = function(df) {
-        function(xvar, yvar, fillby = "1", pt_size = 1, linew = 0.7,
-                 add_cnt_to_legend = TRUE, legend_title = fillby,
+        function(xvar, yvar, colorby = "1", pt_size = 1, linew = 0.7,
+                 add_cnt_to_legend = TRUE, legend_title = colorby,
                  legend_pos = "right", font_size = 14) {
 
                 # --- Prep  --- #
@@ -48,8 +48,8 @@ mk_lineplot = function(df) {
 
                 # --- Main Plot --- #
 
-                p = ggplot(df, aes_string(xvar, yvar, color = fillby)) +
-                        geom_line(aes_string(group = fillby),
+                p = ggplot(df, aes_string(xvar, yvar, color = colorby)) +
+                        geom_line(aes_string(group = colorby),
                                   size = linew, alpha = 0.8) +
                         geom_point(size = pt_size, alpha = 0.8)
 
@@ -75,16 +75,16 @@ mk_lineplot = function(df) {
 
                 # --- Format Legend --- #
 
-                if (fillby == "1") { # remove legend
+                if (colorby == "1") { # remove legend
                         p = p + guides(color = FALSE, fill = FALSE)
                 } else {
                         if (add_cnt_to_legend) {
                                 # count number of non-NA observations for each
-                                #   level of the fillby variable, and make
+                                #   level of the colorby variable, and make
                                 #   new legend label to include these counts
-                                subdf = na.omit(df[c(xvar, yvar, fillby)])
-                                tmp = dplyr::count(subdf, !!as.name(fillby))
-                                legend_txt = paste(tmp[[fillby]],
+                                subdf = na.omit(df[c(xvar, yvar, colorby)])
+                                tmp = dplyr::count(subdf, !!as.name(colorby))
+                                legend_txt = paste(tmp[[colorby]],
                                                    paste0("(n = ", tmp$n, ")"))
 
                                 # use colorblind-friendly colors and update
--- inst/examples/ex-mk_lineplot.R
+++ inst/examples/ex-mk_lineplot.R
@@ -10,9 +10,9 @@ add_labs(p, xlab = NULL, ylab = "boxoffice/budget",
 
 # plot total budget and boxoffice over the years
 plt = mk_lineplot(btbo_by_year)
-plt("year", "tot", fillby = "type")
-plt("year", "tot", fillby = "type", legend_title = NULL, legend_pos = "bottom")
-p = plt("year", "tot", fillby = "type", legend_title = NULL, legend_pos = "top",
+plt("year", "tot", colorby = "type")
+plt("year", "tot", colorby = "type", legend_title = NULL, legend_pos = "bottom")
+p = plt("year", "tot", colorby = "type", legend_title = NULL, legend_pos = "top",
         add_cnt_to_legend = F, font_size = 10)
 p = add_labs(p, xlab = NULL, ylab = "total amount ($billion)",
              title = "Annual Total Budget and Boxoffice from 1913 to 2014",
