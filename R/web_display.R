457d1fbd4cc17c27ad7a58bff9c8a8072c6524dd
Guangming Lang <rigor6vigor@gmail.com>
2019-11-05 22:10:36 +0900

    delete `ggplot2::` since we already import the entire pkg ggplot2 inside ezplot-package.R

--- R/web_display.R
+++ R/web_display.R
@@ -8,7 +8,8 @@
 #' @param p A ggplot object.
 #' @param axis_text_size A number, size of the tick text. Default = 16.
 #' @param axis_title_size A number, size of the axis labels. Default = rel(2).
-#' @param axis_title_vjust_y A number, distance between y label and y-axis. Default = 1.
+#' @param axis_title_vjust_y A number, distance between y label and y-axis.
+#' Default = 1.
 #' @param mar_top A number, top margin of the whole plot. Default = 1.
 #' @param mar_right A number, right margin of the whole plot. Default = 1.
 #' @param mar_left A number, left margin of the whole plot. Default = 1.
@@ -18,25 +19,25 @@
 #'
 #' @export
 #' @examples inst/examples/ex-web_display.R
-web_display = function(p, axis_text_size = 16, axis_title_size = ggplot2::rel(2),
+web_display = function(p, axis_text_size = 16, axis_title_size = rel(2),
                        axis_title_vjust_y = 1, mar_top = 1, mar_right = 1,
                        mar_left = 1, mar_bottom = 1) {
-        p = p + ggplot2::theme(
-                axis.text.x = ggplot2::element_text(size=axis_text_size),
-                axis.text.y = ggplot2::element_text(size=axis_text_size),
-                axis.title.x = ggplot2::element_text(size=axis_title_size,
-                                                     vjust=-axis_title_vjust_y),
-                axis.title.y = ggplot2::element_text(size=axis_title_size,
-                                                     vjust=axis_title_vjust_y),
+        p = p + theme(
+                axis.text.x = element_text(size=axis_text_size),
+                axis.text.y = element_text(size=axis_text_size),
+                axis.title.x = element_text(size=axis_title_size,
+                                            vjust=-axis_title_vjust_y),
+                axis.title.y = element_text(size=axis_title_size,
+                                            vjust=axis_title_vjust_y),
 
-                plot.title = ggplot2::element_text(size=axis_title_size),
+                plot.title = element_text(size=axis_title_size),
 
-                legend.text = ggplot2::element_text(size=axis_text_size),
-                legend.title = ggplot2::element_text(size=axis_title_size),
+                legend.text = element_text(size=axis_text_size),
+                legend.title = element_text(size=axis_title_size),
 
                 plot.margin = grid::unit(
                         c(mar_top, mar_right, mar_left, mar_bottom), "lines")
-        )
+                )
 
         p
 }
