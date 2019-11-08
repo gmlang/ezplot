24b8b12547b73be321faef24d6a943fd6cbd7c9f
Guangming Lang <rigor6vigor@gmail.com>
2019-11-08 12:23:36 +0900

    dropped `detrend` param and make the x and y units and limits the same when making qq plots.

--- R/retired/mk_qqplot.R
+++ R/retired/mk_qqplot.R
@@ -6,7 +6,7 @@
 #'
 #' @param df A data frame.
 #' @return
-#' \code{function(varname, dist = "norm", dparams = list(),
+#' \code{function(varname, dist = "norm", dparams = list(), detrend = TRUE,
 #'                ci_band_type = "pointwise", font_size = 14)}
 #' \itemize{
 #'      \item varname : string, name of a continuous variable. We're interested
@@ -22,6 +22,12 @@
 #'                      function. If an empty list is provided (default), the
 #'                      distributional parameters are estimated via MLE.
 #'                      Default = list().
+#'      \item detrend : logical, Should the resulting Q-Q plot be detrended?
+#'                      If TRUE, it'll be detrended according to the reference
+#'                      Q-Q line. This procedure was described by Thode (2002),
+#'                      and it reduces visual bias caused by orthogonal
+#'                      distances from Q-Q points to the reference line.
+#'                      Default = TRUE.
 #'      \item ci_band_type : string, type of the confidence bands to be drawn:
 #'              "pointwise" (default), "boot", "ks", and "ts", where
 #'              * "pointwise" - simultaneous confidence bands based on the normal distribution;
@@ -38,44 +44,51 @@
 #' @export
 #' @examples inst/examples/ex-mk_qqplot.R
 mk_qqplot = function(df) {
-        function(varname, dist = "norm", dparams = list(),
+        function(varname, dist = "norm", dparams = list(), detrend = TRUE,
                  ci_band_type = "pointwise", font_size = 14) {
 
                 # --- Prep  --- #
 
-                # set x y limits based on the range of the sample values
-                # lim = c(floor(min(df[[varname]], na.rm = TRUE)),
-                #         ceiling(max(df[[varname]], na.rm = TRUE)))
-                lim = pretty(df[[varname]])[c(1, 5)]
-
                 # calc sample size
                 tot_n = length(na.omit(df[[varname]]))
+                ssize = paste0("(n = ", tot_n, ")")
+
+                # get default plot type
+                plot_type = ifelse(dist == "norm", "Q-Q normal plot", "Q-Q plot")
+
+                if (detrend) { # change ylab and plot type
+                        # y is observed sample value - expected value under the
+                        #       chosen theoretical distribution
+                        #       ref: https://github.com/aloy/qqplotr/blob/master/R/stat_qq_point.R
+                        ylab = "Deviations (Observed - Expected)"
+                        plot_type = paste("Detrended", plot_type)
+                }
 
                 # make default subtitle
-                subtit = paste("Q-Q plot", paste0("(n = ", tot_n, ")"))
+                subtit = paste(plot_type, ssize)
+
 
                 # --- Main Plot --- #
 
                 p = ggplot(df, aes_string(sample = varname, fill = "1"))
-                p = p + qqplotr::stat_qq_point(distribution = dist,
-                                               dparams = dparams,
-                                               shape = 16) +
-                        qqplotr::stat_qq_line(distribution = dist,
-                                              dparams = dparams) +
-                        qqplotr::geom_qq_band(distribution = dist,
+                p = p + qqplotr::geom_qq_band(distribution = dist,
+                                              detrend = detrend,
                                               dparams = dparams,
                                               bandType = ci_band_type,
                                               alpha = 0.4) +
-                        # make 1-unit on x-axis equal to 1-unit on y-axis, also
-                        # make x-axis and y-axis have the same limits. This will
-                        # eliminate visual bias.
-                        coord_equal(ylim = lim, xlim = lim)
-
+                        qqplotr::stat_qq_line(distribution = dist,
+                                              detrend = detrend,
+                                              dparams = dparams) +
+                        qqplotr::stat_qq_point(distribution = dist,
+                                               detrend = detrend,
+                                               dparams = dparams,
+                                               shape = 16)
 
                 # --- Format Legend --- #
 
                 # remove legend
                 p = p + guides(color = FALSE, fill = FALSE)
+
 
 
                 # --- Customize Theme --- #
