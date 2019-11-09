d7f7e23f594f57c648005c81ebb6d62952266179
Guangming Lang <rigor6vigor@gmail.com>
2019-11-09 21:27:14 +0900

    add `linew`, curve from exponential model, calc rate based on sample data, color different curves differenlty.

--- R/test_expdist.R
+++ R/test_expdist.R
@@ -1,24 +1,28 @@
-#' @title Create a function for drawing publishable ggplot2 CDF and log10(CCDF)
-#' plots side by side on one canvas. The resulting figure is used to test if
-#' the distribution of the data is exponential.
+#' @title Create a function for drawing publishable ggplot2 CDF and CCDF (on
+#' log-y scale) plots side by side on one canvas. The resulting figure is used
+#' to test if the data distribution is exponential.
 #'
 #' @description
 #' \code{test_expdist} takes a data frame as input and returns a function for
-#' drawing CDF plot and log10(CCDF) plot of any continuous variable
-#' from the data frame. CCDF standands for Complement CDF. If log10(CCDF) is
-#' linear, the observed data are exponentially distributed.
+#' making CDF and CCDF (on log-y scale) plots of any continuous variable
+#' from the data frame. CCDF standands for Complement CDF. If CCDF on log-y
+#' scale is linear, the observed data are exponentially distributed.
 #'
 #' @param df A data frame.
 #' @return
-#' \code{function(varname, xlab = varname, title_left, title_right,
-#'                caption_left, caption_right, xscale_left, ...)}
+#' \code{function(varname, linew = 0.7, xlab = varname, title_left, title_right,
+#'                subtitle_left, subtitle_right, caption_left, caption_right,
+#'                xscale_left, ...)}
 #' \itemize{
 #'      \item varname. String, name of a continuous variable. Its empirical CDF
 #'      will be plotted along side its complement CDF.
+#'      \item linew. Number, width of the line. Default = 0.7.
 #'      \item xlab. String, x label of the left and the right figures.
 #'      Default is varname.
 #'      \item title_left. String, title of the left figure.
 #'      \item title_right. String, title of the right figure.
+#'      \item subtitle_left. String, subtitle of the left figure.
+#'      \item subtitle_right. String, subtitle of the right figure.
 #'      \item caption_left. String, caption of the left figure.
 #'      \item caption_right. String, caption of the right figure.
 #'      \item xscale_left. String, scale of x-axis of the left figure. Possible
@@ -39,10 +43,23 @@ test_expdist = function(df) {
 
         draw_cdf = mk_cdfplot(df)
 
-        function(varname, xlab = varname, title_left, title_right,
-                 caption_left, caption_right, xscale_left, ...) {
+        function(varname, linew = 0.7, xlab = varname, title_left, title_right,
+                 subtitle_left, subtitle_right, caption_left, caption_right,
+                 xscale_left, ...) {
 
-                # --- prep --- #
+                # --- prep data --- #
+
+                cdf = get_cdfs(df)(varname)
+                xs = df[[varname]]
+                ys = log(1 - cdf(xs))
+                df_logCCDF = data.frame(x = xs, y = ys) %>%
+                        dplyr::filter(is.finite(y))
+                fit = lm(y ~ x, data = df_logCCDF)
+                slope = round(setNames(coef(fit)['x'], NULL), 4)
+                rate = abs(slope)
+                avg = round(1 / rate, 2) # mean time between events
+
+                # --- prep args and fig elements --- #
 
                 if (missing(title_left)) {
                         tit1 = paste("Cumulative Distribution of", varname)
@@ -68,11 +85,37 @@ test_expdist = function(df) {
                         cap2 = caption_right
                 }
 
+                if (missing(subtitle_left)) {
+                        subtit1 = paste0(
+                                'The smooth curve is from Exp(', rate, '), ',
+                                'with mean time between events = ', avg, '.\n',
+                                'The stepwise curve is from the data.')
+                } else {
+                        subtit1 = subtitle_left
+                }
+
+                if (missing(subtitle_right)) {
+                        subtit2 = paste0(
+                                'If the trend is linear, the data can be ',
+                                'approximated by \nthe exponential ',
+                                'distribution with rate = ', rate, '.')
+                } else {
+                        subtit2 = subtitle_right
+                }
+
+                txt = paste('Slope:', slope)
+                txt_pos = setNames(c(pretty(xs, 8)[6], 1), c('x', 'y'))
 
                 # --- left figure --- #
 
-                p1 = draw_cdf(varname, legend_pos = 'top', ...) %>%
-                        add_labs(xlab = xlab, title = tit1, caption = cap1)
+                p1 = draw_cdf(varname, legend_pos = 'top', linew = linew, ...) +
+                        stat_function(fun = pexp, args = list(rate = rate),
+                                      alpha = 0.8, size = linew,
+                                      color = '#F28E2B' # color-blind friendly orange
+                                      )
+                p1 = add_labs(p1, xlab = xlab, title = tit1,
+                              subtitle = subtit1, caption = cap1)
+
                 if (missing(xscale_left)) {
                         # don't change x scale, only divide x-axis to 8 ticks
                         p1 = scale_axis(p1, 'x', nticks = 8)
@@ -85,12 +128,21 @@ test_expdist = function(df) {
                 # don't add median vline for CCDF plot regardless user input
                 if (exists('add_vline_median')) add_vline_median = FALSE
                 p2 = draw_cdf(varname, complement = TRUE, legend_pos = 'top',
-                              ...) %>%
-                        # apply log10 scale on y-axis, divide y-axis to 8 ticks
-                        scale_axis(scale = 'log10', nticks = 6) %>%
+                              linew = linew, ...) %>%
+                        # apply log scale on y-axis, divide y-axis to 8 ticks
+                        scale_axis(scale = 'log', nticks = 6) %>%
                         # don't change x scale, only divide x-axis to 8 ticks
                         scale_axis('x', nticks = 8) %>%
-                        add_labs(xlab = xlab, title = tit2, caption = cap2)
+                        add_labs(xlab = xlab, title = tit2, subtitle = subtit2,
+                                 caption = cap2)
+                p2 = p2 + ggplot2::geom_line(stat = "smooth", method = "lm",
+                                             alpha = 0.8, size = linew,
+                                             linetype = 'longdash',
+                                             color = '#F28E2B' # color-blind friendly orange
+                                             ) +
+                        annotate("text", label = txt,
+                                 x = txt_pos['x'], y = txt_pos['y'],
+                                 size = 5)
 
                 cowplot::plot_grid(square_fig(p1), square_fig(p2))
         }
--- inst/examples/ex-test_expdist.R
+++ inst/examples/ex-test_expdist.R
@@ -2,31 +2,26 @@ library(ezplot)
 library(dplyr)
 
 f = test_expdist(iris)
-f("Sepal.Length") # not exponential since the CCDF plot is non-linear
-f("Sepal.Length", add_vline_median = TRUE)
-f("Sepal.Length", add_vline_median = TRUE, show_label_median = FALSE)
-
-f = test_expdist(films)
-f("boxoffice", colorby = 'made_money', font_size = 10)
-
-f1 = test_expdist(films %>% filter((made_money == 'no')))
-f1("boxoffice", font_size = 10, pad = F, xscale_left = 'log10',
-   caption_right = 'Boxoffice is not exponentially distributed at the lower end.')
+f("Sepal.Length")
+f("Sepal.Length", add_vline_median = TRUE, show_label_median = FALSE,
+  caption_left = 'Clearly, sepal length from the sample is not exponential.',
+  subtitle_right = 'Non-linear trend implies data are not exponential')
 
 
 # generate 1000 random values from a exponential distribution of rate 0.2
 df = data.frame(exp_vals = rexp(1000, rate = 0.2))
 f = test_expdist(df)
-f('exp_vals', pad = F,
-  xlab = 'x',
+f('exp_vals', pad = F, xlab = 'x',
   title_left = 'Empirical CDF of 1000 random numbers \n drawn from Exponential(rate = 0.2)',
   title_right = 'Their CCDF on log-y scale looks like this:')
+
 
 # Is the interarrival times between births exponential?
 df = births %>% filter(!is.na(diffs))
 f = test_expdist(df)
-f('diffs',
-  add_vline_median = TRUE, xlab = 'Minutes between births',
+f('diffs', add_vline_median = TRUE, xlab = 'Minutes between births',
+  linew = 1.5,
   title_left = 'CDF of interarrival times between births',
-  title_right = 'Are these interarrival times exponential?',
-  caption_right = 'Not exponential, but can be approximated by an exponential model upto 80 minutes.')
+  title_right = 'Are these interarrival times exponential?')
+
+
